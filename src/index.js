7a958a84badabd2132213ab0c20f2cd716f1fabb
marion586 <marionmenye@gmail.com>
2022-11-02 18:01:38 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -3,23 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-//https://rickandmortyapi.com/graphql
+import { Provider } from "react-redux";
+import { store } from "./state/store";
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://rickandmortyapi.com/graphql",
-  cache: new InMemoryCache(),
-});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
